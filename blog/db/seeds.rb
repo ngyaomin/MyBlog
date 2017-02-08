21f5c42b7ac08dff3c6fbc288f14e22e19ba98cc
Unknown <ngyaomin@ymail.com>
2017-02-08 18:03:52 +0800

    updated css

--- blog/db/seeds.rb
+++ blog/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+posts = Post.create([{title: 'This is a demo title', author: 'Demo Author', sayings: 'How to meditate for enlightenment.
+
+First of all let us define what is enlightenment. Enlightenment is not about being able to create the world or destroy the world, or live forever or controlling the weather. All these are not enlightenments they are mundane siddhis that serve no real purpose beside temporal benefits.
+
+The real enlightenment is the full realisation of the nature of the mind. that is the only enlightenment.
+Of cause they are various levels of realisation of that nature. Now please note that i am not enlighten so i do not know
+if one is enlighten or not. the only way i know if one is enlighten is thought their actions and their teachings.
+For example , but so far he has not shown any sign of full enlightenment in his teachings
+(whether he is or not is beyond my ability to tell, but according to what is manifested from his teachings his enlightenment
+is a low level one, but never the less is still consider enlightnment, just not full). for logical deductions,
+once u grimpse the nature of the mind once you can be consider a saint, people like jesus, Lao ZI are consider saints,
+i have no idea if mother terresa has seen the nature of the mind, but given her contributions she still deserve the saint hood.
+but having this is far from enligthenment.  if enlgithenent can be measured by scores, the arhat enlightenment level
+is an ocean then the level of a god in the highest realm of
+the heavens is only one drop of water. the arhat has rid the five poisons but even that is not complete enlightenment,
+but versus god level they are already well beyond. As buddhist and all religion it is important to respect other religions,
+as each person has his or her own path. '}])
