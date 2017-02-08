281ef447a97f4cd9f9be8c31d5c328d6ff5396d5
Unknown <ngyaomin@ymail.com>
2017-02-08 18:52:50 +0800

    created file for validation

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,16 +1,4 @@
 
 class Post < ApplicationRecord
-  validates :title, presence: true,
-                    length: { maximum: 75 },
-                    uniqueness: true
 
-  validates :author, presence: true,
-                     length: { minimum: 5 },
-                     uniqueness: true,
-                     format: { with: /\A[a-zA-Z]+\z/,
-                     message: "only allows letters" }
-
-  validates :sayings, presence: true,
-                      length: { minimum: 250, maximum:2000 },
-                      uniqueness: true
 end
