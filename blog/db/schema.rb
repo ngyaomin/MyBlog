4336661eaf64ca7260b473a2548a6b33d8706fd1
Unknown <ngyaomin@ymail.com>
2017-02-09 13:52:43 +0800

    create model, migrate, add relationship to comment class.

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208043850) do
+ActiveRecord::Schema.define(version: 20170209054248) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.string   "email"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
