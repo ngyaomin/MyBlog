39a63fb1a7f73e117d447f334a42c1abee522cdc
Unknown <ngyaomin@ymail.com>
2017-02-09 21:42:30 +0800

    better validation and error messages

--- blog/app/models/comment.rb
+++ blog/app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  validates :email, presence: true, format: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+                    message: "give a valid email please." }
   validates :body, presence: true
 
 end
